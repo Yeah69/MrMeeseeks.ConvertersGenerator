26d07d3f18402e2c724fddb351798a4bf83e5175
Dima Enns <dieter.enns@gmail.com>
2024-07-13 08:43:43 +0200

    WIP

---
+++ Main/Generator.cs
@@ -0,0 +1,62 @@
+﻿using System.Globalization;
+using System.Text;
+using Microsoft.CodeAnalysis;
+
+namespace MrMeeseeks.ConvertersGenerator;
+
+internal record OptionsInfo(string RootNamespace);
+
+[Generator]
+public sealed class Generator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        var compilationProvider = context.CompilationProvider;
+        
+        context.RegisterSourceOutput(compilationProvider, (spc, _) =>
+        {
+            spc.AddSource("Generated.cs", "// Hello World!");
+        });
+        
+        var analyzerConfigOptionsProvider = context.AnalyzerConfigOptionsProvider;
+        context.RegisterImplementationSourceOutput(analyzerConfigOptionsProvider, (spc, configOptions) =>
+        {
+            var code = new StringBuilder();
+            code.AppendLine("/*");
+            
+            foreach (var globalOptionsKey in configOptions.GlobalOptions.Keys)
+            {
+                code.AppendFormat(CultureInfo.InvariantCulture, "Key:   {0}", globalOptionsKey);
+                code.AppendLine();
+                if (configOptions.GlobalOptions.TryGetValue(globalOptionsKey, out var value))
+                {
+                    code.AppendFormat(CultureInfo.InvariantCulture, "Value: {0}", value);
+                    code.AppendLine();
+                }
+                code.AppendLine();
+            }
+            
+            code.AppendLine("*/");
+            
+            spc.AddSource("GlobalConfigOptions.g.cs", code.ToString());
+        });
+        
+        context.RegisterPostInitializationOutput(c =>
+        {
+            c.AddSource(
+                "Attribute.cs", 
+                """
+                using System;
+                
+                namespace MrMeeseeks.ConvertersGenerator
+                {
+                    [AttributeUsage(AttributeTargets.Class)]
+                    public class WpfConvertersAttribute : Attribute
+                    {
+                        
+                    }
+                }
+                """);
+        });
+    }
+}
---
+++ Sample.Wpf/Converters.cs
@@ -0,0 +1,12 @@
+﻿using MrMeeseeks.ConvertersGenerator;
+
+namespace Sample.Wpf;
+
+[WpfConverters]
+internal static partial class Converters
+{
+    internal static object Convert(object value, Type targetType)
+    {
+        return value;
+    }
+}
